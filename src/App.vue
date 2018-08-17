9725c26ab15cd442b3e96d1e6867721bc11ade3f
oezcy <oezcan.altin@gmail.com>
2018-08-17 22:38:31 +0200

    Playground (Introduction without 'Composing Components')

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
 <template>
   <div id="app">
     <h1>{{title}}</h1>
-    
+    <vue-playground/>
   </div>
 </template>
 
 <script>
 
-export default {
-  name: 'App',
-  data: function(){
-    return {
-      title: 'ShoppTeam'
+  import VuePlayground from './components/VuePlayground';
+
+  export default {
+    name: 'App',
+    data: function(){
+      return {
+        title: 'ShoppTeam'
+      }
+    },
+    components: {
+      VuePlayground
     }
   }
-}
 
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ src/components/VuePlayground.vue
@@ -0,0 +1,65 @@
+<template>
+  <fieldset>
+    <legend><b>{{title}}</b></legend>
+
+    <p>
+      <span v-bind:title="time">
+        Hover your mouse over me to see actual time.
+      </span>
+    </p>
+
+    <p>
+      <input type="checkbox" v-model="seeText" />
+      <span v-if="seeText">Ja, du willst mich!</span>
+    </p>
+
+    <p>
+      <ul>
+        <li v-for="b in burgers">{{b.name}} <button v-on:click="deleteBurger(b)">X</button></li>
+      </ul>
+      <button v-on:click="addHamburger">Hamburger</button>
+    </p>
+
+    <p>
+      <input type="text" v-model="customText"/>
+      <button v-on:click="addCustomBurger(customText)">Ok</button>
+    </p>
+
+
+  </fieldset>
+</template>
+
+<script>
+export default {
+  name: 'VuePlayground',
+  data () {
+    return {
+      title: 'Vue Playground',
+      time: '' + new Date().toLocaleString(),
+      seeText: false,
+      burgers: [
+        {name:'Big Mac'},
+        {name:'Big King XXL'},
+        {name:'Long Cheese Chilli Chicken'}
+      ]
+    }
+  },
+  methods: {
+    addHamburger: function(){
+      this.burgers.push({name:'Hamburger'});
+    },
+    addCustomBurger: function(cstmText){
+      this.burgers.push({name:cstmText});
+      this.customText = '';
+    },
+    deleteBurger: function(burger){
+      let idx = this.burgers.indexOf(burger);
+      this.burgers.splice(idx,1);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
