4e2564971fd9b8efb3550ce0325415db5745c19c
oezcy <oezcan.altin@gmail.com>
2018-09-21 16:52:30 +0200

    Favorites working

--- src/App.vue
+++ src/App.vue
@@ -8,18 +8,51 @@
     </b-navbar>
      -->
 
-    <b-tabs v-model="tabIndex"
-      v-hammer:swipe.left="onSwipeLeft" >
+    <b-tabs pills v-model="tabIndex">
 
+      <!-- **** FAVORITEN ***** -->
       <b-tab title="Favoriten">
+        <div v-if="this.refloading" align="center" style="width:100%;">
+          <loading-spinner ></loading-spinner>
+        </div>
+        <div v-else>
+          <b-list-group v-if="favorites.length > 0">
+            <b-list-group-item v-for="f in favorites"
+              class="d-flex justify-content-between align-items-center"
+              style="padding: 4px 0px; padding-left: 20px;"
+              @click="onFavoriteClicked(f)"
+              v-bind:class="{ isUsed: isFavInitems(f) }">
+              {{f.title}}
+              <b-btn @click="removeFav(i)" variant="danger" size="sm"
+              style="background-color: #ff7c89;">x</b-btn>
+            </b-list-group-item>
+          </b-list-group>
+          <lazy-smiley v-else></lazy-smiley>
+        </div>
+
+        <b-navbar toggleable="false" type="light" fixed="bottom"
+          v-hammer:swipe.left="onSwipeLeft"
+          v-hammer:swipe.right="onSwipeRight"
+          style="background-color: #4cb817;">
+          <b-nav-form @submit="addNewFav" style="width: 100%;">
+            <b-input-group >
+              <b-form-input v-model="newFavTitle" placeholder="Ihr Favorit" size="" required type="text" />
+              <b-input-group-append>
+                <b-btn type="submit" variant="secondary" size="">Ok</b-btn>
+              </b-input-group-append>
+            </b-input-group>
+          </b-nav-form>
+        </b-navbar>
 
       </b-tab>
 
+      <!-- ****** EINKÄUFE ******* -->
       <b-tab title="Einkäufe">
         <!-- Item liste -->
-        <div v-if="loading" align="center" style="width:100%;">
+        <div v-if="itemloading" align="center" style="width:100%;">
           <loading-spinner ></loading-spinner>
         </div>
+
         <div v-else>
           <b-list-group v-if="items.length > 0">
             <b-list-group-item v-for="i in items"
@@ -33,6 +66,18 @@
           <lazy-smiley v-else></lazy-smiley>
         </div>
 
+        <b-navbar toggleable="false" type="light" variant="info" fixed="bottom"
+          v-hammer:swipe.left="onSwipeLeft"
+          v-hammer:swipe.right="onSwipeRight">
+          <b-nav-form @submit="addNewItem" style="width: 100%;">
+            <b-input-group >
+              <b-form-input v-model="newItemTitle" placeholder="Was musst du kaufen?" size="" required type="text" />
+              <b-input-group-append>
+                <b-btn type="submit" variant="secondary" size="">Ok</b-btn>
+              </b-input-group-append>
+            </b-input-group>
+          </b-nav-form>
+        </b-navbar>
 
       </b-tab>
 
@@ -40,16 +85,7 @@
 
 
     <!-- New item input field -->
-    <b-navbar toggleable="false" type="light" variant="info" fixed="bottom">
-      <b-nav-form @submit="addNewItem" style="width: 100%;">
-        <b-input-group >
-          <b-form-input v-model="newItemTitle" placeholder="Was musst du kaufen?" size="" required type="text" />
-          <b-input-group-append>
-            <b-btn type="submit" variant="secondary" size="">Ok</b-btn>
-          </b-input-group-append>
-        </b-input-group>
-      </b-nav-form>
-    </b-navbar>
+
 
   </div>
 </template>
@@ -74,7 +110,8 @@
   let app = Firebase.initializeApp(config);
   let db = app.database();
 
-  let itemRef = db.ref('items')
+  let itemRef = db.ref('items');
+  let favRef = db.ref('favorites');
 
   export default {
     name: 'App',
@@ -82,17 +119,25 @@
       items: {
         source: itemRef,
         readyCallback: function(){
-          this.loading = false;
+          this.itemloading = false;
+        }
+      },
+      favorites : {
+        source: favRef,
+        readyCallback: function(){
+          this.refloading = false;
         }
       }
     },
     data: function(){
       return {
-        loading : true,
+        itemloading : true,
+        refloading : true,
         title: 'Dringende Einkäufe',
         newItemTitle: '',
+        newFavTitle: '',
         numberOfTabs: 2,
-        tabIndex : 1
+        tabIndex : 1  //intial showed tab (index)
       }
     },
 
@@ -113,6 +158,14 @@
       removeItem: function(item){
         itemRef.child(item['.key']).remove();
       },
+      addNewFav: function(evt){
+        evt.preventDefault();
+        favRef.push({title: this.newFavTitle});
+        this.newFavTitle = '';
+      },
+      removeFav: function(fav){
+        favRef.child(fav['.key']).remove();
+      },
       onSwipeLeft: function(){
         this.tabIndex--;
         if(this.tabIndex < 0){
@@ -124,6 +177,17 @@
         if(this.tabIndex == this.numberOfTabs){
           this.tabIndex = 0;
         }
+      },
+      onFavoriteClicked: function (fav){
+        let newItem = {title: fav.title, favId: fav['.key']};
+        itemRef.push(newItem);
+      },
+      isFavInitems: function (fav){
+        for (var i = 0, len = this.items.length; i < len; i++) {
+        if (this.items[i].favId === fav['.key'])
+            return true; // Return as soon as the object is found
+        }
+        return false; // The object was not found
       }
     }
   }
@@ -146,6 +210,11 @@
   .siteHeader{
     padding: 20px 10px;
     background-color: #c6c6f5; /* light blue */
+  }
+
+  .isUsed{
+    background-color: #e7e6e6;
+    color: white;
   }
 
   body{
