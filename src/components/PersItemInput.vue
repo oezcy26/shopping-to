7345650caab1dea6328f80f7d1d0e6163159fb9b
Altin Özcan <ALO@tfbern.ch>
2019-03-02 00:47:27 +0100

    (small improvemtent) persItemINput

--- src/components/PersItemInput.vue
+++ src/components/PersItemInput.vue
@@ -1,18 +1,17 @@
 <template>
   <v-layout justify-center fluid>
     <v-flex grow>
-      <v-form @submit="addPersItem">
-        <v-text-field
-          v-model="newPersItemTitle"
-          placeholder="Was musst du kaufen?"
-          size
-          required
-          type="text"
-        />
-      </v-form>
+      <v-text-field
+        v-model="newPersItemTitle"
+        placeholder="Was musst du kaufen?"
+        size
+        required
+        type="text"
+        @keydown.enter="addPersItem"
+      />
     </v-flex>
     <v-flex shrink>
-      <v-btn @click="$emit('persitemadded', newPersItemTitle)" fab small color="lime">
+      <v-btn @click="addPersItem" fab small color="lime">
         <v-icon>add</v-icon>
       </v-btn>
     </v-flex>
@@ -29,7 +28,8 @@ export default {
   },
   methods: {
     addPersItem: function() {
-      this.$emit = ("persitemadded", this.newPersItemTitle);
+      let commit = this.newPersItemTitle;
+      this.$emit("persitemadded", commit);
       this.newPersItemTitle = "";
     }
   }
